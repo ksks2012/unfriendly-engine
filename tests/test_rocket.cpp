43400daf290a65d9a5ee40de86cf9ea543e68af3
ksks2012 <st890751999@gmail.com>
2025-04-24 18:03:40 +0800

    test(rocket): Update RocketTest to use unique_ptr for Rocket and MockRenderObjects

--- tests/test_rocket.cpp
+++ tests/test_rocket.cpp
@@ -1,88 +1,92 @@
-// #include "mock.h"
-#include "rendering/render_object.h"
 #include "core/rocket.h"
+#include "rendering/render_object.h"
+#include "logging/logger.h"
+#include "test.h"
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
 
-class MockRenderObject : public IRenderObject {
-public:
-    // Using legacy Google Mock syntax
-    MOCK_CONST_METHOD0(render, void());
-    MOCK_CONST_METHOD0(getVao, GLuint());
-    MOCK_CONST_METHOD0(getVbo, GLuint());
-    MOCK_CONST_METHOD3(renderTrajectory, void(size_t, size_t, size_t));
-    MOCK_METHOD3(updateBuffer, void(GLintptr, GLsizei, const void*));
-};
+using ::testing::_;
+using ::testing::AtLeast;
 
 class RocketTest : public ::testing::Test {
-  protected:
+protected:
     Config config;
-    Rocket *rocket;
+    std::unique_ptr<Rocket> rocket;
+    std::shared_ptr<MockLogger> logger;
 
-    void SetUp() override
-    {
+    std::unique_ptr<MockRenderObject> mockRenderObject;
+    std::unique_ptr<MockRenderObject> mockTrajectory;
+    std::unique_ptr<MockRenderObject> mockPrediction;
+
+    void SetUp() override {
         config = Config();
-        rocket = new Rocket(config, FlightPlan(config.flight_plan_path));
-    }
+        logger = std::make_shared<MockLogger>();
+        EXPECT_CALL(*logger, log(_, _, _)).Times(AtLeast(0));
+        EXPECT_CALL(*logger, set_level(_)).Times(AtLeast(0));
 
-    void TearDown() override { delete rocket; }
+
+        mockRenderObject = std::make_unique<MockRenderObject>();
+        mockTrajectory = std::make_unique<MockRenderObject>();
+        EXPECT_CALL(*mockTrajectory, updateBuffer(_, _, _)).Times(AtLeast(0));
+        mockPrediction = std::make_unique<MockRenderObject>();
+        EXPECT_CALL(*mockPrediction, updateBuffer(_, _, _)).Times(AtLeast(0));
+            
+        rocket = std::make_unique<Rocket>(config, logger, FlightPlan(config.flight_plan_path));
+    }
 };
 
-TEST_F(RocketTest, Initialization)
-{
+TEST_F(RocketTest, Initialization) {
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().x, 0.0f);
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().y, 0.0f);
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().z, 0.0f);
-
-    auto mockRender = std::make_unique<MockRenderObject>();
-    auto mockTrajectory = std::make_unique<MockRenderObject>();
-    auto mockPrediction = std::make_unique<MockRenderObject>();
-
-    rocket->setRenderObjects(std::move(mockRender), std::move(mockTrajectory),
-                             std::move(mockPrediction));
-
+    
+    rocket->setRender(std::move(mockRenderObject));
+    rocket->setTrajectoryRender(std::move(mockTrajectory), std::move(mockPrediction));
     rocket->init();
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().x, 0.0f);
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().y, 1.0f);
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().z, 0.0f);
 }
 
-TEST_F(RocketTest, InitInjectsMockRenderObject)
-{
+TEST_F(RocketTest, InitInjectsMockRenderObject) {
     auto mockRender = std::make_unique<MockRenderObject>();
     auto mockTrajectory = std::make_unique<MockRenderObject>();
     auto mockPrediction = std::make_unique<MockRenderObject>();
 
-    rocket->setRenderObjects(std::move(mockRender), std::move(mockTrajectory),
-                             std::move(mockPrediction));
+    rocket->setRender(std::move(mockRenderObject));
+    rocket->setTrajectoryRender(std::move(mockTrajectory), std::move(mockPrediction));
     EXPECT_NO_THROW(rocket->init());
 }
 
-TEST_F(RocketTest, OffsetPosition_Default)
-{
-    config.rocket_initial_position =
-        glm::vec3(0.0f, 6371000.0f, 0.0f);      // Earth's radius in meters
-    config.simulation_rendering_scale = 0.001f; // Scale for rendering
+TEST_F(RocketTest, OffsetPosition_Default) {
+    config.rocket_initial_position = glm::vec3(0.0f, 6371000.0f, 0.0f);
+    config.simulation_rendering_scale = 0.001f;
+
+    // Recreate rocket with updated config
+    rocket = std::make_unique<Rocket>(config, logger, FlightPlan(config.flight_plan_path));
 
     glm::vec3 offset = rocket->offsetPosition();
 
     EXPECT_FLOAT_EQ(offset.x, 0.0f);
-    EXPECT_FLOAT_EQ(offset.y, 6371.0f); // Earth's radius scaled
+    EXPECT_FLOAT_EQ(offset.y, 6371.0f);
     EXPECT_FLOAT_EQ(offset.z, 0.0f);
 }
 
-TEST_F(RocketTest, OffsetPosition_CustomPosition)
-{
-    config.rocket_initial_position =
-        glm::vec3(1000.0f, 6372000.0f, 2000.0f); // Custom position
-    config.simulation_rendering_scale = 0.001f;  // Scale for rendering
+TEST_F(RocketTest, OffsetPosition_CustomPosition) {
+    config.rocket_initial_position = glm::vec3(1000.0f, 6372000.0f, 2000.0f);
+    config.simulation_rendering_scale = 0.001f;
+
+    // Recreate rocket with updated config
+    rocket = std::make_unique<Rocket>(config, logger, FlightPlan(config.flight_plan_path));
 
     glm::vec3 customPosition = glm::vec3(2000.0f, 6373000.0f, 4000.0f);
     glm::vec3 offset = rocket->offsetPosition(customPosition);
 
-    EXPECT_FLOAT_EQ(offset.x, 2.0f);    // Scaled x
-    EXPECT_FLOAT_EQ(offset.y, 6373.0f); // Scaled altitude
-    EXPECT_FLOAT_EQ(offset.z, 4.0f);    // Scaled z
+    EXPECT_FLOAT_EQ(offset.x, 2.0f);
+    EXPECT_FLOAT_EQ(offset.y, 6373.0f);
+    EXPECT_FLOAT_EQ(offset.z, 4.0f);
 }
 
 TEST_F(RocketTest, FlightPlanExecution) {
@@ -95,24 +99,15 @@ TEST_F(RocketTest, FlightPlanExecution) {
         }
     )"_json;
     FlightPlan plan(json);
-    rocket = new Rocket(config, plan);
+    rocket = std::make_unique<Rocket>(config, logger, plan);
 
-    auto mockRender = std::make_unique<MockRenderObject>();
-    auto mockTrajectory = std::make_unique<MockRenderObject>();
-    auto mockPrediction = std::make_unique<MockRenderObject>();
-
-    auto *mockTrajectoryRaw = mockTrajectory.get();
-    EXPECT_CALL(*mockTrajectoryRaw, updateBuffer(testing::_, testing::_, testing::_)).Times(testing::AtLeast(0));
-
-    rocket->setRenderObjects(std::move(mockRender), std::move(mockTrajectory),
-                             std::move(mockPrediction));
+    rocket->setRender(std::move(mockRenderObject));
+    rocket->setTrajectoryRender(std::move(mockTrajectory), std::move(mockPrediction));
 
     rocket->init();
     rocket->launched = true;
 
-    rocket->trajectorySampleTime = -999.0f; // ignore trajectory
-    rocket->update(0.1f, BODY_MAP());
-
+    rocket->update(0.1f, {});
     EXPECT_FLOAT_EQ(rocket->getThrustDirection().y, 1.0f);
     EXPECT_FLOAT_EQ(rocket->thrust, 25000000.0f);
 }
